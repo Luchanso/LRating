14c80a28af91c4c38813c2f3817b86cae44e73fe
Loutchansky Oleg <oleg@loutchansky.com>
2015-03-16 21:00:02 +0300

    Add IServer.hx

---
+++ LRatingServer/src/com/luchanso/lrating/IServer.hx
@@ -0,0 +1,15 @@
+package com.luchanso.lrating;
+
+import com.luchanso.lrating.Score;
+
+/**
+ * @author Loutchansky Oleg
+ */
+
+interface IServer 
+{
+	public function newRecord(score:Score, game:String, hashSumm:String, serverHash:String, serverKey:String):Bool;
+	public function getTableRecords(game:String):Map<Int, Score>;
+	public function getServerHash():String;
+	public function getServerKey():String;	
+}
