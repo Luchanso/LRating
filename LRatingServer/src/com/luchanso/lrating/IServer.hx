145cbe8f9926b496666c4cc7d71ef8cb32b1bb8f
Loutchansky Oleg <oleg@loutchansky.com>
2015-03-17 10:07:27 +0300

    Update IServer add function getServerData

--- LRatingServer/src/com/luchanso/lrating/IServer.hx
+++ LRatingServer/src/com/luchanso/lrating/IServer.hx
@@ -10,6 +10,5 @@ interface IServer
 {
 	public function newRecord(score:Score, game:String, hashSumm:String, serverHash:String, serverKey:String):Bool;
 	public function getTableRecords(game:String):Map<Int, Score>;
-	public function getServerHash():String;
-	public function getServerKey():String;	
+	public function getServerData():Dynamic;
 }
