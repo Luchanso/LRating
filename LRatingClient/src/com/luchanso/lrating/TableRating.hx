76e05c6c5b09267f5c26e6092e8adb03ede41b5e
Loutchansky Oleg <oleg@loutchansky.com>
2015-03-19 19:14:27 +0300

    Remove trash and add Animation

--- LRatingClient/src/com/luchanso/lrating/TableRating.hx
+++ LRatingClient/src/com/luchanso/lrating/TableRating.hx
@@ -5,12 +5,16 @@ import com.luchanso.lrating.TableRating.Server;
 import haxe.crypto.Md5;
 import haxe.remoting.AsyncProxy;
 import haxe.remoting.HttpAsyncConnection;
+import motion.Actuate;
+import motion.easing.Bounce;
 import openfl.display.Sprite;
-import openfl.events.Event;
+import openfl.events.KeyboardEvent;
 import openfl.events.MouseEvent;
+import openfl.Lib;
 import openfl.text.TextField;
 import openfl.text.TextFieldAutoSize;
 import openfl.text.TextFormat;
+import openfl.ui.Keyboard;
 
 class Server extends AsyncProxy<com.luchanso.lrating.server.IServer> {
 }
@@ -20,43 +24,29 @@ class Server extends AsyncProxy<com.luchanso.lrating.server.IServer> {
  * @author Loutchansky Oleg
  */
 class TableRating extends Sprite
-{
+{	
+	static var alphaMax = 0.93;
+	static var animationTime = .55;
 	
-	static inline var font = "Arial";
-	static inline var heSize = 15;
-	static inline var heX = 15;
-	
-	static inline var hrThickness = 1;
 	
 	public var connectionCallback:Void -> Void;
+	
+	var currentWidth = Lib.current.stage.stageWidth;
+	var currentHeight = Lib.current.stage.stageHeight;
 	
 	var api 		:Server;
 	var serverHash 	:String;
 	var serverKey 	:String;
 	var privateKey	:String;
 	
-	var colorHeader	:Int;
-	var colorScores	:Int;
-	var heHeight	:Float;
-	var rowHeight	:Float;
-	
 	var gameName	:String;
+	var them		:Them;
 	
-	/**
-	 * Score list
-	 */
 	public var scores:Map<Int, Score>;
+	public var scoreSprites:List<ScoreSprite>;
 	
-	/**
-	 * Background color
-	 */
-	public var bgColor:Int;
-	/**
-	 * Header text
-	 */
-	public var headerText(default, set):Float;
-	
-	var header:TextField;
+	var header	:TextField;
+	var info 	:TextField;
 	
 	/**
 	 * Visual table rating
@@ -70,41 +60,96 @@ class TableRating extends Sprite
 	 * @param	rowHeight
 	 * @param	alpha
 	 */
-	public function new(gameName:String, width:Float, height:Float, headerText:String, bgColor:Int = 0xFFFFFF, colorHeader:Int = 0x56009D, 
-						colorScores:Int = 0xEC0000,	hrColor:Int = 0xBCBCBC, rowHeight:Float = 30, alpha:Float = 1)
+	public function new(gameName:String, fontName:String = "Arial", them:Them = null)
 	{
 		super();
 		
-		this.gameName = gameName;
+		if (them == null)
+			this.them = them = new BlueThem();			
+		else
+			this.them = them;
 		
-		this.alpha = alpha;
-		this.colorHeader = colorHeader;
-		this.colorScores = colorScores;
-		this.heHeight = rowHeight;
-		this.rowHeight = rowHeight;
+		
+		this.gameName = gameName;
+		this.alpha = 0;
+		this.visible = false;
 		
 		scores = new Map<Int, Score>();
+		scoreSprites = new List<ScoreSprite>();
+		
+		graphics.beginFill(them.backgroundTableColor);
+		graphics.drawRect(0, 0, currentWidth, currentHeight);
+		graphics.endFill();
 		
 		header = new TextField();
-		header.defaultTextFormat = new TextFormat(font, heSize, this.colorHeader);
-		header.text = headerText;
+		header.defaultTextFormat = new TextFormat(fontName, 30, them.tableColor);
+		header.text = "Рейтинг";
 		header.autoSize = TextFieldAutoSize.LEFT;
-		header.x = heX;
-		header.y = heHeight / 2 - header.height / 2;
+		header.x = currentWidth / 2 - header.width / 2;
+		header.y = 0;
 		header.selectable = false;
 		header.mouseEnabled = false;
 		
-		graphics.beginFill(bgColor);
-		graphics.drawRect(0, 0, width, rowHeight - 1);
-		graphics.endFill();
-		
-		graphics.lineStyle(hrThickness, hrColor);
-		graphics.moveTo(0, heHeight);
-		graphics.lineTo(width, heHeight);
+		info = new TextField();
+		info.defaultTextFormat = new TextFormat(fontName, 25, them.tableColor);
+		info.text = "Нажмите пробел, чтобы продолжить";
+		info.autoSize = TextFieldAutoSize.LEFT;
+		info.x = currentWidth / 2 - info.width / 2;
+		info.y = currentHeight - info.height - 5;
+		info.selectable = false;
+		info.mouseEnabled = false;
 		
 		addChild(header);
+		addChild(info);
+	}
+	
+	private function keyDown(e:KeyboardEvent):Void 
+	{
+		if (e.keyCode == Keyboard.SPACE)
+		{
+			this.hide();
+			this.dispatchEvent(new LRatingEvent(LRatingEvent.TABLE_CLOSE));
+		}
+	}
+	
+	public function hide()
+	{
+		var isCancel = this.dispatchEvent(new LRatingEvent(LRatingEvent.TABLE_HIDE, false, true));
 		
-		this.addEventListener(MouseEvent.MOUSE_MOVE, mouseMove);
+		if (isCancel)
+		{
+			Lib.current.stage.removeEventListener(MouseEvent.MOUSE_MOVE, mouseMove);
+			Lib.current.stage.removeEventListener(KeyboardEvent.KEY_DOWN, keyDown);			
+			
+			var i = 0;
+			for (sprite in scoreSprites)
+			{				
+				sprite.hide().delay(i/1000 * 25);
+				i++;
+			}			
+			
+			Actuate.tween(this, animationTime, { alpha: 0 } ).autoVisible().delay((currentHeight / 50)/1000 * 25);
+		}
+	}
+	
+	public function show()
+	{
+		var isCancel = this.dispatchEvent(new LRatingEvent(LRatingEvent.TABLE_SHOW, false, true));
+		
+		if (isCancel)
+		{			
+			Lib.current.stage.addEventListener(MouseEvent.MOUSE_MOVE, mouseMove);
+			Lib.current.stage.addEventListener(KeyboardEvent.KEY_DOWN, keyDown);
+			//this.visible = true;
+			Actuate.tween(this, animationTime, { alpha: alphaMax } ).autoVisible();
+			
+			var i = 0;
+			for (sprite in scoreSprites)
+			{
+				sprite.show().delay(animationTime + i/1000 * 50);
+				i++;
+			}
+		}
 	}
 	
 	private function mouseMove(e:MouseEvent):Void 
@@ -127,17 +172,21 @@ class TableRating extends Sprite
 	 * @param	url
 	 */
 	public function addScore(username:String, score:Float, position:Int, url:String = null)
-	{
+	{		
 		var score = new Score(username, score, position, url);
 		
+		var scoreSprite = new ScoreSprite(score, currentWidth - currentWidth / 3, 50, them);
+		scoreSprite.x = currentWidth / 2 - scoreSprite.width / 2;		
+		scoreSprite.y = 50 + scoreSprite.height * Lambda.count(scores) + 5;
 		
-		var scoreSprite = new ScoreSprite(score, width - 1, rowHeight, 13, colorScores);
-		scoreSprite.x = 0;
-		scoreSprite.y = heHeight + rowHeight * Lambda.count(scores) + hrThickness;		
+		scoreSprites.add(scoreSprite);
 		
 		scores.set(position, score);
 		
 		addChild(scoreSprite);
+		
+		if (this.visible)
+			scoreSprite.show();
 	}
 	
 	public function addToServer(score:Score)
@@ -192,11 +241,6 @@ class TableRating extends Sprite
 		this.serverHash = data.serverHash;
 		
 		connectionCallback();
-	}
-	
-	function set_headerText(value:Float):Float 
-	{
-		return headerText = value;
 	}
 	
 }
