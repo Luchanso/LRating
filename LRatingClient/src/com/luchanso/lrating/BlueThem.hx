89b811bbc62c5e3bfaae0fae008bfa2e0ba7afbf
Loutchansky Oleg <oleg@loutchansky.com>
2015-03-19 19:07:40 +0300

    Add BlueThem.hx

---
+++ LRatingClient/src/com/luchanso/lrating/BlueThem.hx
@@ -0,0 +1,18 @@
+package com.luchanso.lrating;
+
+/**
+ * ...
+ * @author Loutchansky Oleg
+ */
+class BlueThem extends Them
+{
+	public function new()
+	{
+		this.backgroundTableColor = 0x0680FF;
+		this.backgroundScoreColor = 0x5A00F7;
+		this.backgroundScorePageColor = 0x597DA3;
+		this.tableColor = 0xFFFFFF;
+		this.scoreColor = 0xFFFFFF;
+		this.scorePageColor = 0xFFFFFF;
+	}
+}
