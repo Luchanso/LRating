5f3ea74186f680dc846c59285b3e16b277e372cf
Loutchansky Oleg <oleg@loutchansky.com>
2015-03-19 19:14:37 +0300

    Add Them.hx

---
+++ LRatingClient/src/com/luchanso/lrating/Them.hx
@@ -0,0 +1,15 @@
+package com.luchanso.lrating;
+
+/**
+ * ...
+ * @author Loutchansky Oleg
+ */
+class Them
+{	
+	public  var backgroundTableColor = 0;
+	public  var backgroundScoreColor = 0;
+	public  var backgroundScorePageColor = 0;
+	public  var tableColor = 0;
+	public  var scoreColor = 0;
+	public  var scorePageColor = 0;
+}
