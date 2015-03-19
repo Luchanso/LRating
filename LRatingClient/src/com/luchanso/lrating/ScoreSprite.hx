cc596e101d94c8e4d546596c26fd1217acd7e01b
Loutchansky Oleg <oleg@loutchansky.com>
2015-03-19 19:13:29 +0300

    Add show, hide functions and them

--- LRatingClient/src/com/luchanso/lrating/ScoreSprite.hx
+++ LRatingClient/src/com/luchanso/lrating/ScoreSprite.hx
@@ -1,8 +1,13 @@
 package com.luchanso.lrating;
 
 
+import motion.Actuate;
+import motion.actuators.GenericActuator;
+import openfl.display.GradientType;
 import openfl.display.Sprite;
 import openfl.events.MouseEvent;
+import openfl.filters.DropShadowFilter;
+import openfl.geom.Matrix;
 import openfl.Lib;
 import openfl.net.URLRequest;
 import openfl.text.TextField;
@@ -16,7 +21,7 @@ import openfl.text.TextFormat;
 class ScoreSprite extends Sprite
 {
 	static inline var fFont = "Arial";
-	static inline var paddingRL = 15;	
+	static inline var paddingRL = 10;
 	static inline var margin = 5;
 	static inline var hrThickness = 1;
 	
@@ -24,13 +29,17 @@ class ScoreSprite extends Sprite
 	var tfUsername:TextField;
 	var tfScore:TextField;
 	
+	var sPositionX : Float;
+	var sPositionY : Float;
+	
 	public var url:String;
 	public var score:Score;
 
-	public function new(score:Score, width:Float, height:Float, fontSize = 13, color = 0x191919, background = 0xFFFFFF, hrColor = 0xBCBCBC)
+	public function new(score:Score, width:Float, height:Float, them:Them)
 	{
 		super();
 		
+		this.alpha = 0;
 		this.score = score;
 		this.url = score.url;
 		
@@ -42,17 +51,16 @@ class ScoreSprite extends Sprite
 			this.addEventListener(MouseEvent.CLICK, urlOpen);
 		}
 		
-		graphics.beginFill(background);
-		graphics.drawRect(0, 0, width - 1, height - 1);
+		var matrix = new Matrix();
+		matrix.createGradientBox(width, height);
+		
+		graphics.beginGradientFill(GradientType.LINEAR, [them.backgroundScoreColor, them.backgroundScoreColor], [1, 0], [0, 255], matrix);
+		graphics.drawRect(0, 0, width, height);
 		graphics.endFill();
 		
-		graphics.lineStyle(hrThickness, hrColor);
-		graphics.moveTo(0, height);
-		graphics.lineTo(width, height);		
-		
 		tfPosition = new TextField();
-		tfPosition.defaultTextFormat = new TextFormat(fFont, fontSize, color);
-		tfPosition.text = score.position + ")";
+		tfPosition.defaultTextFormat = new TextFormat(fFont, 21, them.scoreColor);
+		tfPosition.text = Std.string(score.position);
 		tfPosition.autoSize = TextFieldAutoSize.LEFT;
 		tfPosition.x = paddingRL;
 		tfPosition.y = height / 2 - tfPosition.height / 2;
@@ -60,7 +68,7 @@ class ScoreSprite extends Sprite
 		tfPosition.mouseEnabled = false;
 		
 		tfUsername = new TextField();
-		tfUsername.defaultTextFormat = new TextFormat(fFont, fontSize, color);
+		tfUsername.defaultTextFormat = new TextFormat(fFont, 21, them.scoreColor);
 		tfUsername.text = score.username;
 		tfUsername.autoSize = TextFieldAutoSize.LEFT;
 		tfUsername.x = tfPosition.x + tfPosition.width + margin;
@@ -69,7 +77,7 @@ class ScoreSprite extends Sprite
 		tfUsername.mouseEnabled = false;
 		
 		tfScore = new TextField();
-		tfScore.defaultTextFormat = new TextFormat(fFont, fontSize, color);
+		tfScore.defaultTextFormat = new TextFormat(fFont, 21, them.scoreColor);
 		tfScore.text = Std.string(score.score);
 		tfScore.autoSize = TextFieldAutoSize.RIGHT;
 		tfScore.x = this.width - tfScore.width - paddingRL;
@@ -80,6 +88,25 @@ class ScoreSprite extends Sprite
 		addChild(tfPosition);
 		addChild(tfUsername);
 		addChild(tfScore);
+	}
+	
+	public function show() : GenericActuator<ScoreSprite>
+	{
+		sPositionX = this.x;
+		sPositionY = this.y;
+		
+		this.x = sPositionX - this.width / 2;
+		this.y = sPositionY - this.height / 2;
+		
+		return Actuate.tween(this, 1, { alpha : 1, x: sPositionX, y: sPositionY } ).autoVisible();
+	}
+	
+	public function hide() : GenericActuator<ScoreSprite>
+	{
+		this.x = sPositionX;
+		this.y = sPositionY;
+		
+		return Actuate.tween(this, 1, { alpha : 0 } ).autoVisible();
 	}
 	
 	private function urlOpen(e:MouseEvent):Void 
